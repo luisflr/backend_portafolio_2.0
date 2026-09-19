36df3a9009da242938a21f1e1dc40ed1b2a1fa3a
luisflr <lfloresr@ulasalle.edu.pe>
2026-09-19 15:37:20 -0500

    added whitenoise

--- backend_portafolio_2/settings.py
+++ backend_portafolio_2/settings.py
@@ -180,6 +180,7 @@ MEDIA_URL = '/media/'
 # Carpeta física en tu servidor donde se guardarán los archivos
 MEDIA_ROOT = BASE_DIR / 'media'
 
+WHITENOISE_ROOT = MEDIA_ROOT
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 

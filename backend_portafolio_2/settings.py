683bc3a13bdfc8cb56796b6511f60be5cfbb3faa
luisflr <lfloresr@ulasalle.edu.pe>
2026-09-19 01:24:51 -0500

    Migracion a psql

--- backend_portafolio_2/settings.py
+++ backend_portafolio_2/settings.py
@@ -78,11 +78,19 @@ WSGI_APPLICATION = 'backend_portafolio_2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=False,  # interna de Railway no requiere SSL
+    )
 }
 
 

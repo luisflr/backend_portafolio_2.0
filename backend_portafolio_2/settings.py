10780c342f241ea2c4e443c863da895bcea1b6d9
luisflr <lfloresr@ulasalle.edu.pe>
2026-09-19 14:20:23 -0500

    configure settings

--- backend_portafolio_2/settings.py
+++ backend_portafolio_2/settings.py
@@ -39,6 +39,7 @@ ALLOWED_HOSTS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://luisflr.com",
+    "https://portafolio20-pearl.vercel.app/",
 ]
 
 CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
